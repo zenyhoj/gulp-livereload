7a1918c3122160631c3718f31fc759fd58ee9dc4
zenyhoj <joe.balingit@gmail.com>
2018-12-12 13:37:40 +0800

    added plugin for creating css source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
+"use strict";
+
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
-var pump = require("pump");
+
 var livereload = require("gulp-livereload");
 var concat = require("gulp-concat"); //css concatenation package
-var minifyCss = require('gulp-minify-css'); //minify css
-var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require("gulp-minify-css"); //minify css
+var autoprefixer = require("gulp-autoprefixer");
+var plumber = require("gulp-plumber");
+var notify = require("gulp-notify");
+var sourcemaps = require('gulp-sourcemaps');
+
 // file paths
 var DIST_PATH = "public/dist";
 var SCRIPTS_PATH = "public/scripts/**/*.js";
 var CSS_PATH = "public/css/**/*.css";
 
-
 gulp.task("html", function () {
   console.log("html started");
-
 });
 
 gulp.task("styles", function () {
@@ -23,9 +27,18 @@ gulp.task("styles", function () {
       "public/css/reset.css",
       CSS_PATH
     ]) /** load reset.css first then the rest*/
+    .pipe(
+      plumber(function (err) {
+        console.log("Styles Task Error");
+        console.log(err);
+        this.emit("end");
+      })
+    )
+    .pipe(sourcemaps.init())
     .pipe(autoprefixer())
     .pipe(concat("styles.css"))
     .pipe(minifyCss())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(DIST_PATH))
     .pipe(livereload());
 });
@@ -46,8 +59,6 @@ gulp.task("watch", function () {
   require("./server"); /* run the server.js */
   livereload.listen();
   // gulp.watch([SCRIPTS_PATH, CSS_PATH, HTML_PATH], ["scripts", "styles", "html"]);
-  gulp.watch(SCRIPTS_PATH, ["scripts"])
+  gulp.watch(SCRIPTS_PATH, ["scripts"]);
   gulp.watch(CSS_PATH, ["styles"]);
-
-
 });
